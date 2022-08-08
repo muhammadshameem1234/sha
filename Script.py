3cb43155c8cace534e62c08d7cca0b66bf364e36
muhammadshameem1234 <108507023+muhammadshameem1234@users.noreply.github.com>
2022-08-08 20:54:42 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -15,13 +15,13 @@ class script(object):
 - our group - https://t.me/+TE-wkN31185hOWJl  
 
 <b>DEVS:</b>
-- <a href=https://t.me/TeamEvamaria>Team Eva Maria</a>"""
+- <a href=https://t.me/shameempalattuparambil>This person</a>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
 - Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
 
 <b>NOTE:</b>
-1. eva maria should have admin privillage.
+1. S2 series should have admin privillage.
 2. only admins can add filters in a chat.
 3. alert buttons have a limit of 64 characters.
 
